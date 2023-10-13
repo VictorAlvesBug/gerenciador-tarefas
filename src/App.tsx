054901a7a60a74fdee7db3613f88ae13ab52ac5a
Victor Alves Bugueno <victor.bugueno@fiap.com.br>
2023-10-13 18:36:39 -0300

    Primeiro Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react';
+import { Tarefa, TTarefa } from './components/Tarefa';
 
 function App() {
+  const [texto, setTexto] = useState('');
+  const [listaTarefas, setListaTarefas] = useState<TTarefa[]>([]);
+
+  const onAdicionar = () => {
+    if (!texto) return;
+    setListaTarefas((prev) => {
+      const novaTarefa: TTarefa = {
+        id: Date.now(),
+        texto: texto,
+        feita: false,
+      };
+      return [...prev, novaTarefa];
+    });
+    setTexto('');
+  };
+
+  const onMarcar = (id: number) => {
+    setListaTarefas((prev) => {
+      return prev.map((tarefa) => {
+        if (tarefa.id === id) {
+          return { ...tarefa, feita: !tarefa.feita };
+        }
+        return tarefa;
+      });
+    });
+  };
+
+  const onExcluir = (id: number) => {
+    setListaTarefas((prev) => {
+      return prev.filter((tarefa) => tarefa.id !== id);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex flex-col justify-start items-center pt-10 bg-gray-200">
+      <div className="border border-gray-500 bg-white w-96 p-5">
+        <div className="flex flex-col justify-evenly items-center border-b border-b-gray-500">
+          <h1 className="text-4xl">Tarefas</h1>
+          <div className="flex flex-row justify-between items-center gap-2 w-full p-3">
+            <input
+              type="text"
+              placeholder="Digite aqui..."
+              value={texto}
+              onChange={(e) => setTexto(e.target.value)}
+              onKeyUp={(e) => e.key === 'Enter' && onAdicionar()}
+              className="bg-gray-200 w-full px-2 py-1 outline-none cursor-pointer"
+            />
+            <button
+              type="button"
+              onClick={onAdicionar}
+              className="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1"
+            >
+              Adicionar
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col justify-evenly items-center p-3">
+          {listaTarefas.length === 0 ? (
+            <p>Nenhuma tarefa encontrada</p>
+          ) : (
+            <>
+              {listaTarefas.map((tarefa) => {
+                return (
+                  <Tarefa
+                    key={tarefa.id}
+                    tarefa={tarefa}
+                    onMarcar={onMarcar}
+                    onExcluir={onExcluir}
+                  />
+                );
+              })}
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Tarefa.tsx
@@ -0,0 +1,63 @@
+import { Tooltip } from 'react-tooltip'
+
+export type TTarefa = {
+  id: number;
+  texto: string;
+  feita: boolean;
+};
+
+type TarefaProps = {
+    tarefa: TTarefa;
+    onMarcar: (id: number) => void;
+    onExcluir: (id: number) => void;
+}
+
+type TClasseCondicional = {
+    classePadrao: string;
+    classeTrue: string;
+    classeFalse?: string;
+}
+
+function getClasse (classeCondicional: TClasseCondicional, condicao: boolean) {
+    return condicao 
+        ? `${classeCondicional.classePadrao} ${classeCondicional.classeTrue}`
+        : `${classeCondicional.classePadrao} ${classeCondicional.classeFalse || ''}`
+}
+
+export function Tarefa({tarefa, onMarcar, onExcluir}: TarefaProps) {
+    const classesCondicionais = {
+        checkbox: {
+            classePadrao: "pr-4 h-4 appearance-none after:absolute transform after:w-4 after:h-4 after:border-2 after:border-blue-600",
+            classeTrue: 'before:absolute before:h-2 before:w-2 before:translate-x-1 before:translate-y-1 before:bg-blue-600',
+        } as TClasseCondicional,
+        texto: {
+            classePadrao: 'overflow-hidden overflow-ellipsis',
+            classeTrue: 'line-through text-gray-400 flex',
+        } as TClasseCondicional,
+    };
+
+  return <div 
+    onClick={e => onMarcar(tarefa.id)}
+    className="w-full flex flex-row justify-start items-center gap-2 py-1">
+        <Tooltip 
+            id={`texto-tarefa-${tarefa.id}`}
+            content={tarefa.texto} 
+            delayShow={300}
+            className='z-10'/>
+    <input 
+        type="checkbox" 
+        checked={tarefa.feita} 
+        className={getClasse(classesCondicionais.checkbox, tarefa.feita)} />
+    <pre 
+        data-tooltip-id={`texto-tarefa-${tarefa.id}`}
+        className={getClasse(classesCondicionais.texto, tarefa.feita)}>
+            <span>{tarefa.texto}</span>
+    </pre>
+    <button 
+        type="button" 
+        onClick={e => onExcluir(tarefa.id)}
+        className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 ml-auto">
+            Excluir
+    </button>
+  </div>;
+}
